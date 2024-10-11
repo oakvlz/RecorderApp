80ef8b213b451cb3795fbbe225eda47ab0f158ab
oakvlz <octopuuzdark@gmail.com>
2024-10-11 00:06:54 -0600

    20241011 0006 se ingresa lector de qr

--- app/src/main/java/com/example/recorderapp/MainActivity.kt
+++ app/src/main/java/com/example/recorderapp/MainActivity.kt
@@ -2,20 +2,28 @@ package com.example.recorderapp
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.example.recorderapp.appgraficas.AppGraficasActivity
 import com.example.recorderapp.databinding.ActivityMainBinding
 import com.example.recorderapp.dipainhouse.DipainHouseActivity
+import com.google.zxing.ResultPoint
 import com.google.zxing.integration.android.IntentIntegrator
+import com.journeyapps.barcodescanner.BarcodeCallback
+import com.journeyapps.barcodescanner.BarcodeResult
+import com.journeyapps.barcodescanner.camera.CameraSettings
+
 
 class MainActivity : AppCompatActivity() {
+    private var isPreview=false
     lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnAppRecorder.setOnClickListener {
             startActivity(Intent(this@MainActivity, RecorderActivity::class.java))
@@ -27,23 +35,61 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this@MainActivity, DipainHouseActivity::class.java))
         }
         binding.btnQr.setOnClickListener {
-            startScan()
+            //startScan()
+            qrCodePreview(true)
+            binding.tvResult.text = ""
+            binding.qrScannerView.decodeSingle(object : BarcodeCallback {
+                override fun barcodeResult(result: BarcodeResult) {
+                    Log.d("qr", "barcode result: $result")
+                    binding.tvResult.text = result.text
+                    qrCodePreview(false)
+                    //binding.qrScannerView.barcodeView.
+                    // startScan()
+                    // do your thing with result
+                }
+                override fun possibleResultPoints(resultPoints: List<ResultPoint>) {}
+            })
         }
         binding.tvResult.setOnClickListener {
             openUrl()
         }
 
+        binding.qrScannerView.isVisible = isPreview
+        val s = CameraSettings()
+        s.requestedCameraId = 0 // front/back/etc
+        s.isAutoFocusEnabled = true
 
+        binding.qrScannerView.barcodeView.cameraSettings = s
+
+
+        /*binding.qrScannerView.decodeContinuous(object : BarcodeCallback {
+            override fun barcodeResult(result: BarcodeResult) {
+                Log.d("qr","barcode result: $result")
+                binding.tvResult.text=result.text
+            }
+            override fun possibleResultPoints(resultPoints: List<ResultPoint>) {}
+        })*/
+    }
+    private fun qrCodePreview(estatus:Boolean){
+        isPreview=estatus
+        binding.qrScannerView.isVisible=isPreview
+        binding.qrScannerView.apply {
+            if (isPreview)
+                resume()
+            else
+                pause()
+        }
     }
 
     private fun startScan() {
         val integrator = IntentIntegrator(this)
-        integrator.setDesiredBarcodeFormats("QR_CODE")
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)//leer todos los codigos
         integrator.setPrompt("Escanea el código QR")
         integrator.setCameraId(0)
-        integrator.setBeepEnabled(false)
-        integrator.setBarcodeImageEnabled(false)
+        integrator.setBeepEnabled(true)
+        integrator.setBarcodeImageEnabled(true)
         integrator.initiateScan()
+
     }
     private fun openUrl() {
         val url = binding.tvResult.text.toString()
@@ -74,39 +120,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    object NGpsMarcador {
-        fun getRecurso(tipo:String):Int{
-            return when(tipo){
-                "147" -> R.drawable.ic_marcador111
-                "148" -> R.drawable.ic_marcador112
-                "149" -> R.drawable.ic_marcador113
-                "157" -> R.drawable.ic_marcador121
-                "158" -> R.drawable.ic_marcador122
-                "159" -> R.drawable.ic_marcador123
-                "167" -> R.drawable.ic_marcador131
-                "168" -> R.drawable.ic_marcador132
-                "169" -> R.drawable.ic_marcador133
-                "247" -> R.drawable.ic_marcador211
-                "248" -> R.drawable.ic_marcador212
-                "249" -> R.drawable.ic_marcador213
-                "257" -> R.drawable.ic_marcador221
-                "258" -> R.drawable.ic_marcador222
-                "259" -> R.drawable.ic_marcador223
-                "267" -> R.drawable.ic_marcador231
-                "268" -> R.drawable.ic_marcador232
-                "269" -> R.drawable.ic_marcador233
-                "347" -> R.drawable.ic_marcador311
-                "348" -> R.drawable.ic_marcador312
-                "349" -> R.drawable.ic_marcador313
-                "357" -> R.drawable.ic_marcador321
-                "358" -> R.drawable.ic_marcador322
-                "359" -> R.drawable.ic_marcador323
-                "367" -> R.drawable.ic_marcador331
-                "368" -> R.drawable.ic_marcador332
-                "369" -> R.drawable.ic_marcador333
-                else -> R.drawable.ic_marcador111
-            }
-        }
-    }
+
 
 }
